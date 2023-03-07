0ab439bc4cf58eaf535ee9ad0e26b22f88fe5375
12rcu <56599453+12rcu@users.noreply.github.com>
2023-03-07 14:57:53 +0100

    added ktor framework

---
+++ API/src/main/kotlin/de/matthiasklenz/Application.kt
@@ -0,0 +1,18 @@
+package de.matthiasklenz
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import de.matthiasklenz.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureHTTP()
+    configureSerialization()
+    configureRouting()
+}
