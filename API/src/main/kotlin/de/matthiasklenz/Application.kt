db7483f937b068f856e0e0b6b217e0c3eafa7a3b
12rcu <56599453+12rcu@users.noreply.github.com>
2023-03-09 15:52:23 +0100

    netty config with commandline args

--- API/src/main/kotlin/de/matthiasklenz/Application.kt
+++ API/src/main/kotlin/de/matthiasklenz/Application.kt
@@ -4,15 +4,33 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import de.matthiasklenz.plugins.*
+import io.ktor.server.config.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+fun main(args: Array<String>) {
+    val applicationEnvironment = commandLineEnvironment(args)
+    NettyApplicationEngine(applicationEnvironment) {
+        loadConfiguration(applicationEnvironment.config)
+    }.start(true)
 }
 
+@Suppress("unused")     //set in application
 fun Application.module() {
     configureSecurity()
     configureHTTP()
     configureSerialization()
     configureRouting()
+}
+
+private fun NettyApplicationEngine.Configuration.loadConfiguration(config: ApplicationConfig) {
+    val deploymentConfig = config.config("ktor.deployment")
+    loadCommonConfiguration(deploymentConfig)
+    deploymentConfig.propertyOrNull("requestQueueLimit")?.getString()?.toInt()?.let {
+        requestQueueLimit = it
+    }
+    deploymentConfig.propertyOrNull("shareWorkGroup")?.getString()?.toBoolean()?.let {
+        shareWorkGroup = it
+    }
+    deploymentConfig.propertyOrNull("responseWriteTimeoutSeconds")?.getString()?.toInt()?.let {
+        responseWriteTimeoutSeconds = it
+    }
 }
